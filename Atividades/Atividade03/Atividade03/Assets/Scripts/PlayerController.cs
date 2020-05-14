9ccf6f1b8b4f3c62d15771a03f9c2c35fb3ed225
josekarnikowski <josekarnikowski@gmail.com>
2020-05-14 09:48:44 -0300

    Criando o projeto Atividade03

---
+++ Atividades/Atividade03/Atividade03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+    public float moveSpeed = 0.2f;
+    public float moveRotation = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if (isLocalPlayer)
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed);
+            transform.Rotate(0, Input.GetAxis("Horizontal") * moveRotation, 0);
+        }
+    }
+}
